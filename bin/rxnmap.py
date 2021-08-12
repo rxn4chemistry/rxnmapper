c6df3a41efbf3a36ad8151bde2983b2fb5f63ed0
Francois Berenger <unixjunkie@sdf.org>
2021-08-12 11:20:34 +0900

    new file:   bin/rxnmap.py

---
+++ bin/rxnmap.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from rxnmapper import RXNMapper
+
+rxn_mapper = RXNMapper()
+rxns = ['CC(C)S.CN(C)C=O.Fc1cccnc1F.O=C([O-])[O-].[K+].[K+]>>CC(C)Sc1ncccc1F', 'C1COCCO1.CC(C)(C)OC(=O)CONC(=O)NCc1cccc2ccccc12.Cl>>O=C(O)CONC(=O)NCc1cccc2ccccc12']
+results = rxn_mapper.get_attention_guided_atom_maps(rxns)
+
+for r in results:
+    print(r)
+
+# [{'mapped_rxn': 'CN(C)C=O.F[c:5]1[n:6][cH:7][cH:8][cH:9][c:10]1[F:11].O=C([O-])[O-].[CH3:1][CH:2]([CH3:3])[SH:4].[K+].[K+]>>[CH3:1][CH:2]([CH3:3])[S:4][c:5]1[n:6][cH:7][cH:8][cH:9][c:10]1[F:11]',
+#   'confidence': 0.9565619900376546},
+#  {'mapped_rxn': 'C1COCCO1.CC(C)(C)[O:3][C:2](=[O:1])[CH2:4][O:5][NH:6][C:7](=[O:8])[NH:9][CH2:10][c:11]1[cH:12][cH:13][cH:14][c:15]2[cH:16][cH:17][cH:18][cH:19][c:20]12.Cl>>[O:1]=[C:2]([OH:3])[CH2:4][O:5][NH:6][C:7](=[O:8])[NH:9][CH2:10][c:11]1[cH:12][cH:13][cH:14][c:15]2[cH:16][cH:17][cH:18][cH:19][c:20]12',
+#   'confidence': 0.9704424331552834}]
