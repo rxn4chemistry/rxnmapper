c5e8da82074e55df9b026cb7027f6a6c82d9ad57
Alain Vaucher <ava@zurich.ibm.com>
2022-08-04 19:12:46 +0200

    Bump version: 0.1.4 → 0.2.0

--- rxnmapper/__init__.py
+++ rxnmapper/__init__.py
@@ -1,6 +1,6 @@
 """rxnmapper initialization."""
 __name__ = "rxnmapper"
-__version__ = "0.1.4"  # managed by bump2version
+__version__ = "0.2.0"  # managed by bump2version
 
 
 from .core import RXNMapper  # noqa
