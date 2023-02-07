ea4dc87000a91d25459d537db0f7d2b8b3ad814c
Alain Vaucher <ava@zurich.ibm.com>
2023-02-07 12:50:28 +0100

    update variable name

--- tests/utils.py
+++ tests/utils.py
@@ -3,20 +3,20 @@ from typing import Any, Dict, Iterable
 import numpy as np
 
 
-def assert_correct_map(result: Dict[str, Any], exp: Dict[str, Any]) -> None:
+def assert_correct_map(value_1: Dict[str, Any], value_2: Dict[str, Any]) -> None:
     mandatory_keys = ["mapped_rxn", "confidence"]
 
     # Exact matches
     for key in ["mapped_rxn", "pxr_mapping_vector", "tokens"]:
-        if key not in mandatory_keys and key not in result:
+        if key not in mandatory_keys and key not in value_1:
             continue
-        assert result[key] == exp[key]
+        assert value_1[key] == value_2[key]
 
     # close match on single number
     for key in ["confidence"]:
-        if key not in mandatory_keys and key not in result:
+        if key not in mandatory_keys and key not in value_1:
             continue
-        assert np.isclose(result[key], exp[key])
+        assert np.isclose(value_1[key], value_2[key])
 
     # close match on multiple values
     for key in [
@@ -25,9 +25,9 @@ def assert_correct_map(result: Dict[str, Any], exp: Dict[str, Any]) -> None:
         "tokensxtokens_attns",
         "mapping_tuples",
     ]:
-        if key not in mandatory_keys and key not in result:
+        if key not in mandatory_keys and key not in value_1:
             continue
-        assert np.allclose(result[key], exp[key], rtol=1e-4, atol=1e-7)
+        assert np.allclose(value_1[key], value_2[key], rtol=1e-4, atol=1e-7)
 
 
 def assert_correct_maps(
